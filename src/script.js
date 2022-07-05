1f15d3004402e517cbb122edffac317f949f26b9
Azhar Beg <99490876+azhar-beg@users.noreply.github.com>
2022-07-05 14:40:31 +0530

    Working game

--- src/script.js
+++ src/script.js
@@ -11,16 +11,9 @@ class Person {
     this.#y = y;
   }
 
-  moveUp(delta) {
-    this.#y -= delta;
-  }
-
-  moveRight(delta) {
-    this.#x += delta;
-  }
-
-  moveLeft(delta) {
-    this.#x -= delta;
+  movePerson(dx, dy) {
+    this.#x += dx;
+    this.#y += dy;
   }
 
   getDetails() {
@@ -75,9 +68,8 @@ const moveBricks = (bricks, speed) => {
   })
 };
 
-const addNewBrick = (id, bricks) => {
-  const x = between(0, 170)
-  const brick = new Brick(id, x, 348);
+const addNewBrick = (id, bricks, x, y) => {
+  const brick = new Brick(id, x, y);
   drawBrick(brick);
   bricks.push(brick);
 };
@@ -89,7 +81,6 @@ const onTopEdge = (brick) => {
 
 const removeBrick = (brick) => {
   const { id } = brick.getDetails();
-  console.log(id);
   const element = document.getElementById(id);
   element.remove()
 };
@@ -103,54 +94,93 @@ const removeBricksOnEdge = bricks => {
   })
 };
 
-const movePersonUp = (person, speed) => {
-  person.moveUp(speed);
-  const { y, id } = person.getDetails();
-  const element = document.getElementById(id);
-  element.style.top = y;
-};
-
 class Game {
   #bricks;
   #person;
-  #speed;
-  constructor(bricks, person, speed) {
+  #view;
+  constructor(bricks, person, speed, view) {
     this.#bricks = bricks;
     this.#person = person;
-    this.#speed = speed;
+    this.#view = view;
   }
 
   isOver() {
     const { y } = this.#person.getDetails();
-    return y === 0 || y === 329;
+    return y <= 0 || y >= 329;
   }
 
+  isPersonOnBrick() {
+    return this.#bricks.some(brick => {
+      const person = this.#person.getDetails();
+      const { x, y } = brick.getDetails();
+      return y === person.y + 25 && (person.x + 18 >= x && person.x <= (x + 25));
+    })
+  }
+
+  personDetails() {
+    return this.#person.getDetails();
+  };
+
+  updatePerson(dx, dy, present) {
+    this.#person.movePerson(dx, dy);
+    present(this.#person);
+  }
+
+  isPersonInScreen() {
+
+  }
+}
+
+const updatePosition = person => {
+  const { y, x, id } = person.getDetails();
+  const element = document.getElementById(id);
+  element.style.top = y;
+  element.style.left = x;
+};
+
+function movePerson(game) {
+  return (event) => {
+    if (event.key == 'ArrowLeft') {
+      game.updatePerson(-3, 0, updatePosition);
+    }
+    else if (event.key == 'ArrowRight') {
+      game.updatePerson(3, 0, updatePosition)
+    }
+  };
 }
 
 const main = () => {
-  // const view = { x: 0, y: 0, height: 400, width: 300 };
-  const brick = new Brick(1, 10, 348);
-  const person = new Person('person', 20, 328);
+  const view = { x: 0, y: 0, height: 352, width: 180 };
+  const brick = new Brick(1, 12, 350);
+  const person = new Person('person', 30, 325);
   const bricks = [brick];
-  const game = new Game(bricks, person, 2);
-
+  const game = new Game(bricks, person, view);
   drawBrick(brick);
+
+  const speed = 1;
   let idCounter = 1;
   let counter = 0;
-  let speed = 2;
 
   setInterval(() => {
     if (game.isOver()) {
       return;
     }
     counter++;
+
+    if (game.isPersonOnBrick()) {
+      person.movePerson(0, -speed);
+    } else { person.movePerson(0, speed) }
+    updatePosition(person)
+
     moveBricks(bricks, speed);
-    movePersonUp(person, speed);
-    if (counter % 80 === 0) {
-      addNewBrick(++idCounter, bricks)
+    if (counter % 70 === 0) {
+      const x = between(0, 150)
+      addNewBrick(++idCounter, bricks, x, 348)
     }
     removeBricksOnEdge(bricks)
-  }, 20)
+  }, 20);
+
+  document.addEventListener('keydown', movePerson(game));
 };
 
 window.onload = main;
